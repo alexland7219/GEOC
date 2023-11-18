306ba63589cd13bef408463d2a74c715b8d3dc82
Alexandre Ros <alexland7219@gmail.com>
2023-11-18 16:00:53 +0100

    Added Lab4 files

---
+++ Lab4/triangulation.js
@@ -0,0 +1,22 @@
+/**
+ TODO Replace this by your own, correct, triangulation function
+ Triangles should be return as arrays of array of indexes
+ e.g., [[1,2,3],[2,3,4]] encodes two triangles, where the indices are relative to the array points
+**/
+function computeTriangulation(points) {
+	// Wrong code! Just connects consecutive three after sorting by x-coord
+	// Note that this does NOT return a triangulation, just a subset of it
+	//var newPoints = points.sort(function(a,b) { if ((a.x) < (b.x)) return -1; else return 1;})
+	var k = Math.floor(points.length/3); 
+	var outputTriangles = new Array(k); 
+	
+	for (i=0;i<k;i++) {
+		// This is how one triangle is represented: array of three point indices
+		outputTriangles[i] = [3*i, 3*i+1, 3*i+2]; // Store INDICES, not points
+	}
+
+	return outputTriangles;
+}
+
+
+
