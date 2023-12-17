778190e46dc13026eaa94ee3d2030c3282d9b8b9
Alexandre Ros <alexland7219@gmail.com>
2023-12-17 19:45:02 +0100

    yoyo done

--- video/partIII.py
+++ video/partIII.py
@@ -11,6 +11,18 @@ def compArrows(nodes, edges, color, buf):
 
 
 class GenericElection(Scene):
+    def getSquare(self, text, color, pos, height=15, textcolor=WHITE):
+        content = Text(text, color=textcolor, font="DejaVu Sans Mono").scale(0.4).move_to(pos)
+
+        square = Rectangle(color=color, width=15, height=height)
+        square.set_fill(color, opacity=1)
+
+        square.surround(content, buff=0.2)
+        self.play(*[Create(square), Create(content)])
+
+        return square, content
+
+
     def construct(self):
         self.camera.background_color = "#F9F2E5"
 
@@ -35,7 +47,7 @@ class GenericElection(Scene):
 
         #nodes = [(3, 3), (3.7, 1.5), (3.1, -0.5), (0.7, -0.8), (0, 1), (0.65, 2.2)]
         
-        nodes = [(2.46, 2.73), (3.55, 1.69), (3, -0.5), (0.81, 1.38), (0.8, -0.13), (1.5,2),
+        nodes = [(2.46, 2.73), (3.55, 1.69), (2.6, -0.5), (0.81, 1.38), (0.8, -0.13), (1.5,2),
                  (2.42, 1.56), (1.71, 0.80), (3,0.5), (0.6, 2.3)]
         nodes = [(n[0]-1.5, n[1]-1) for n in nodes]
         nodes = [(2*x, 2*y) for x, y in nodes]
@@ -44,7 +56,8 @@ class GenericElection(Scene):
         nodes = compNodes(nodes, BLACK)
 
         #edges = [(x, (x+1) % len(nodes)) for x in range(len(nodes))]
-        edges = [(0,5), (0,6), (0,1), (6,5), (1,6),(4,3), (3,7), (7,8),(4,8), (8,1),(8,2), (5,3),(6,7),(5,9)]
+        #edges = [(0,5), (0,6), (0,1), (6,5), (1,6),(4,3), (3,7), (7,8),(4,8), (8,1),(8,2), (5,3),(6,7),(5,9)]
+        edges = [(0,1), (5,0), (9,3), (3,5), (6,5), (7,6), (1,8), (3,4), (4,7), (8,7), (2,7), (2,8), (7,5)]
         edges = compEdges(nodes, edges)
 
         self.play(*[Create(x) for x in nodes])
@@ -71,7 +84,7 @@ class GenericElection(Scene):
         setupAlg = [Tex(r"Nodes will ask \textbf{id} of every neighbor.", font_size=120, color=BLACK).scale(0.3).move_to(3*LEFT+1*UP),
                     Tex(r"Every link will interchange two \textbf{id}.", font_size=120, color=BLACK).scale(0.3).move_to(3.1*LEFT+0.5*UP)]
 
-        arrows = [(8,7), (3,7), (4,7), (6,7)]
+        arrows = [(5,7), (6,7), (8,7), (2,7), (4,7)]
         arrows = compArrows(nodes, arrows, BLUE_C, 0.11)
 
         self.play(*[Create(x) for x in setupAlg])
@@ -84,7 +97,7 @@ class GenericElection(Scene):
             Tex(r"Every node will know their edges.", font_size=120, color=BLACK).scale(0.3).move_to(3.1*LEFT+1*DOWN)]
 
 
-        arrows2 = [(8,7), (8,1), (1,0), (2,8), (7,3), (4,8), (4,3), (7,6), (3,5), (6,5), (6,1), (5,0), (6,0),(5,9)]
+        arrows2 = [(1,0), (5,0), (3,9), (3,5), (6,5), (7,6), (8,1), (4,3), (4,7), (8,7), (2,7), (2,8), (7,5)]
         arrows2 = compArrows(nodes, arrows2, BLACK, 0.25)
         self.play(*[Create(c) for c in setupAlg2])
         self.play(*[Uncreate(x) for x in arrows])
@@ -108,8 +121,8 @@ class GenericElection(Scene):
 
         stepName = Text("YO-yo", color=WHITE, font="DejaVu Sans Mono").scale(0.4).move_to(5*LEFT+2*UP)
 
-        square = Rectangle(color=GREEN_C, width=15, height=5)
-        square.set_fill(GREEN_C, opacity=1)
+        square = Rectangle(color=0x009900, width=15, height=5)
+        square.set_fill(0x009900, opacity=1)
 
         square.surround(stepName, buff=0.5)
         self.play(Create(square))
@@ -117,16 +130,16 @@ class GenericElection(Scene):
         self.wait(5)
 
         setupAlg = [Tex(r"Sources $\alpha$ and $\beta$ send their \textbf{id}.", font_size=120, color=BLACK).scale(0.3).move_to(3.5*LEFT+1*UP),
-                    Tex(r"Worst \textbf{id} is propagated downstream.", font_size=120, color=BLACK).scale(0.3).move_to(3.1*LEFT+0.5*UP)]
+                    Tex(r"Lowest \textbf{id} is propagated \textit{downstream}.", font_size=120, color=BLACK).scale(0.3).move_to(3.1*LEFT+0.5*UP)]
         self.play(*[Create(x) for x in setupAlg])
         
-        sources = [(3, -0.5),  (0.8, -0.13)]
+        sources = [(2.6, -0.5),  (0.8, -0.13)]
         sources = [(n[0]-1.5, n[1]-1) for n in sources]
         sources = [(2*x, 2*y) for x, y in sources]
         sources = [(n[0]+2, n[1]-0.4) for n in sources]
 
-        sources = compNodes(sources, RED_C)
-        sources[1].set_color(BLUE_C)
+        sources = compNodes(sources, 0x9900ff)
+        sources[1].set_color(0x009900)
 
         alpha = Tex(r"$\alpha$", font_size=120, color=BLACK).scale(0.3).move_to(nodes[2].get_center() + 0.5*RIGHT)
         beta = Tex(r"$\beta$", font_size=120, color=BLACK).scale(0.3).move_to(nodes[4].get_center() + 0.5*DOWN)
@@ -137,37 +150,39 @@ class GenericElection(Scene):
 
         self.wait(3)
 
-        arrows = [(2,8), (4,8), (4,3)]
-        arrows = compArrows(nodes, arrows, RED_C, 0.25)
+        arrows = [(2,8), (2,7), (4,7), (4,3)]
+        arrows = compArrows(nodes, arrows, 0x9900ff, 0.25)
 
-        arrows[1].set_color(BLUE_C)
-        arrows[2].set_color(BLUE_C)
+        arrows[2].set_color(0x009900)
+        arrows[3].set_color(0x009900)
 
         self.play(*[Create(a) for a in arrows])
         self.wait(3)
 
-        arrows2 = [(8,7),(8,1)]
-        arrows2 = compArrows(nodes, arrows2, RED_C, 0.25)
+        arrows2 = [(8,7), (8,1),(3,5), (3,9)]
+        arrows2 = compArrows(nodes, arrows2, 0x9900ff, 0.25)
+        arrows2[-2].set_color(0x009900)
+        arrows2[-1].set_color(0x009900)
         self.play(*[Create(a) for a in arrows2])
         self.wait(1)
 
-        arrows3 = [(7,3),(7,6)]
-        arrows3 = compArrows(nodes, arrows3, RED_C, 0.25)
+        arrows3 = [(7,5),(7,6), (1,0)]
+        arrows3 = compArrows(nodes, arrows3, 0x9900ff, 0.25)
         self.play(*[Create(a) for a in arrows3])
         self.wait(1)
 
-        arrows4 = [(3,5), (6,5), (6,0), (6,1)]
-        arrows4 = compArrows(nodes, arrows4, RED_C, 0.25)
+        arrows4 = [(6,5)]
+        arrows4 = compArrows(nodes, arrows4, 0x9900ff, 0.25)
         self.play(*[Create(a) for a in arrows4])
         self.wait(1)
 
-        arrows5 = [(5,9), (5,0), (1,0)]
-        arrows5 = compArrows(nodes, arrows5, RED_C, 0.25)
+        arrows5 = [(5,0)]
+        arrows5 = compArrows(nodes, arrows5, 0x9900ff, 0.25)
         self.play(*[Create(a) for a in arrows5])
         self.wait(1)
 
-        gamma = Tex(r"$\gamma$", font_size=120, color=BLACK).scale(0.3).move_to(nodes[3].get_center() + 0.3*LEFT + 0.3*UP)
-        delta = Tex(r"$\delta$", font_size=120, color=BLACK).scale(0.3).move_to(nodes[8].get_center() + 0.5*RIGHT)
+        gamma = Tex(r"$\gamma$", font_size=120, color=BLACK).scale(0.3).move_to(nodes[7].get_center() + 0.4*LEFT+0.2*UP)
+        delta = Tex(r"$\delta$", font_size=120, color=BLACK).scale(0.3).move_to(nodes[5].get_center() + 0.4*UP)
         self.play(Create(gamma))
         self.play(Create(delta))
 
@@ -181,15 +196,97 @@ class GenericElection(Scene):
         self.play(*[Uncreate(x) for x in setupAlg])
         self.play(Uncreate(square))
         self.play(Uncreate(stepName))
+        self.play(Uncreate(gamma))
+        self.play(Uncreate(delta))
 
         stepName = Text("yo-YO", color=WHITE, font="DejaVu Sans Mono").scale(0.4).move_to(5*LEFT+2*UP)
 
-        square = Rectangle(color=PURPLE, width=15, height=5)
-        square.set_fill(PURPLE, opacity=1)
+        square = Rectangle(color=0x9900ff, width=15, height=5)
+        square.set_fill(0x9900ff, opacity=1)
 
         square.surround(stepName, buff=0.5)
         self.play(Create(square))
         self.play(Create(stepName))
         self.wait(5)
 
-        
+        ### yo-YO steop ####
+
+        setupAlg = [Tex(r"Nodes propagate \textit{upstream} signals \textbf{Y}/\textbf{N}.", font_size=120, color=BLACK).scale(0.3).move_to(3.5*LEFT+1*UP),
+            Tex(r"\textbf{Y} sent to edges with local smallest \textbf{id}.", font_size=120, color=BLACK).scale(0.3).move_to(3.45*LEFT),
+            Tex(r"\textbf{N} sent otherwise, or if a \textbf{N} is recieved.", font_size=120, color=BLACK).scale(0.3).move_to(3.45*LEFT + 0.5*DOWN)]
+        self.play(*[Create(x) for x in setupAlg])
+
+        self.wait(4)
+
+        self.getSquare("Y", 0x0033cc, arrows2[-1].get_center())
+        self.getSquare("Y", 0x0033cc, arrows3[-1].get_center())
+        self.getSquare("Y", 0x0033cc, arrows5[0].get_center())
+
+        self.wait(2)
+
+        self.getSquare("Y", 0x0033cc, arrows2[1].get_center())
+        self.getSquare("Y", 0x0033cc, arrows3[0].get_center())
+        self.getSquare("Y", 0x0033cc, arrows4[0].get_center())
+        self.getSquare("N", 0xe60000, arrows2[-2].get_center())
+
+        self.wait(2)
+
+        self.getSquare("N", 0xe60000, arrows[-1].get_center())
+        self.getSquare("Y", 0x0033cc, arrows3[1].get_center())
+
+        self.wait(2)
+
+        self.getSquare("N", 0xe60000, arrows[-2].get_center())
+        self.getSquare("Y", 0x0033cc, arrows2[0].get_center())
+        self.getSquare("Y", 0x0033cc, arrows[1].get_center())
+
+        self.wait(2)
+
+        self.getSquare("Y", 0x0033cc, arrows[0].get_center())
+
+        self.wait(5)
+
+        setupAlg2 = [Tex(r"Reverse the \textbf{N} edges.", font_size=120, color=BLACK).scale(0.3).move_to(4.7*LEFT + 2.5*DOWN),
+        Tex(r"Sources with at least one \textbf{N} die ($\beta$).", font_size=120, color=BLACK).scale(0.3).move_to(3.5*LEFT + 2*DOWN)]
+        self.play(*[Create(x) for x in setupAlg2[::-1]])
+
+        self.wait(5)
+
+        ## PRUNING ##
+
+        self.play(*[Uncreate(x) for x in setupAlg2[::-1]])
+        self.play(*[Uncreate(x) for x in setupAlg])
+        self.play(Uncreate(stepName))
+        self.play(Uncreate(square))
+
+        stepName = Text("PRUNING", color=WHITE, font="DejaVu Sans Mono").scale(0.4).move_to(5*LEFT+2*UP)
+
+        square = Rectangle(color=0xa67b5b, width=15, height=5)
+        square.set_fill(0xa67b5b, opacity=1)
+
+        square.surround(stepName, buff=0.5)
+        self.play(Create(square))
+        self.play(Create(stepName))
+        self.wait(5)
+
+        setupAlg = [Tex(r"\textit{Unary sinks} are useless.", font_size=120, color=BLACK).scale(0.3).move_to(4.4*LEFT+1*UP),
+            Tex(r"\textit{In edges} with same value are useless.", font_size=120, color=BLACK).scale(0.3).move_to(3.45*LEFT + 0.5*UP)]
+        self.play(*[Create(x) for x in setupAlg])
+
+        self.wait(2)
+
+        self.getSquare("PRUNE", 0xa67b5b, nodes[9].get_center(), height=5)
+        self.getSquare("PRUNE", 0xa67b5b, arrows3[-1].get_center(), height=5)
+        self.getSquare("PRUNE", 0xa67b5b, arrows4[-1].get_center(), height=5)
+        self.getSquare("PRUNE", 0xa67b5b, arrows[1].get_center(), height=5)
+
+        self.wait(2)
+
+        setupAlg = [Tex(r"Every iteration of YO-YO kills a source.", font_size=120, color=BLACK).scale(0.3).move_to(3.3*LEFT+1*DOWN),
+            Tex(r"Only the smallest \textbf{id}, $\alpha$, survives.", font_size=120, color=BLACK).scale(0.3).move_to(3.8*LEFT + 1.5*DOWN),
+            Tex(r"Termination when only $\alpha$ isn't pruned.", font_size=120, color=BLACK).scale(0.3).move_to(3.5*LEFT+2*DOWN)]
+        self.play(*[Create(x) for x in setupAlg])
+
+        self.getSquare("LEADER", 0xffef00, nodes[2].get_center()+LEFT, height=5, textcolor=BLACK)
+
+        self.wait(2)
